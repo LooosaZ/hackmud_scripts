7990e68b80a90c60a1120e51aba78cfed89039d4
LoosaZ <losartorii@outlook.com>
2024-05-04 03:20:17 +0100

    names:true gets u 3 locs to use. Just ctrl+v output

--- tier1.js
+++ tier1.js
@@ -1,24 +1,26 @@
-function(e,r){ //t:#s.t1npc.loc0
-
-//	["EZ_21","EZ_35","EZ_40","c001","c002","c003","l0cket","DATA_CHECK","green","lime","yellow","orange","red","purple","blue","cyan","open","release","unlock","6hh8xw","cmppiq","sa23uw","tvfkyq","uphlaw","vc2c7q","xwz7ja","pattern ","device","more","humor","angie's","mallory's","che ","will","faythe","hack","pet","safety","service","fact","capture","friend","th3","provides i","gc","CAFE","fran_lee","robovac","sentience","sans_comedy","angels","minions","sisters","petra","fountain","helpdesk","bunnybat","get_level","weathernet","eve","resource","bo","heard","teach","outta_juice","poetry","match","length","call","indexOf","LOCK_ERROR","includes","color_digit","c002_complement","c003_triad_1","c003_triad_2","digit","prime","ez_prime","split","\n","forEach","state"]	
-let[f,i,s,l,t,a,$,n,o,v,k,u,S,c,p,C,I,L,N,O,_,b,d,g,h,z,D,E,K,P,T,U,m,q,B,J,R,X,j,w,x,y,A,F,G,H,M,Q,V,W,Y,Z,ee,re,fe,ie,se,le,te,ae,$e,ne,oe,ve,ke,ue,Se,ce,pe,Ce,Ie,Le,Ne,Oe,_e,be,de,ge,he,ze,De,Ee,Ke]=JSON.parse(#fs.scripts.quine().split`	`[1]),hack=e=>{var r=[f,i,s,l,t,a,$,n],Pe=[o,v,k,u,S,c,p,C],Te=[I,L,N],Ue=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97],me=[O,_,b,d,g,h,z],qe=[D,E,K,P,T,U,m,q,B,J,R,X,j,w,x,y,A,F,G,H],Be=[M,Q,V,W,Y,Z,ee,re,fe,ie,se,le,te,ae,$e,ne,oe,ve,ke,ue],Je=e
-let open_cnt=e=>(e[Se](/LOCK_UNLOCKED/g)||[])[ce]
-for(var Re={},Xe=Je[pe](Re),je=Xe[Ce](Ie);;){if(-1==je)return[Re,Xe]
-for(var we="",xe=0;r[ce]>xe;xe++)if(Xe[Ce](r[xe])>je){we=r[xe]
-break}var ye=open_cnt(Xe)
-for(xe=0;;xe++){let pullAns=e=>{for(var r=0;qe[ce]>r;r++)e[Le](qe[r])&&(Fe+=Be[r])}
-if(we==l){Re[l]=Pe[xe%Pe[ce]]
-Re[Ne]=Re[l][ce]}else if(we==t){Re[t]=Pe[xe%Pe[ce]]
-Re[Oe]=Pe[(xe+4)%Pe[ce]]}else if(we==a){Re[a]=Pe[xe%Pe[ce]]
-Re[_e]=Pe[(xe+3)%Pe[ce]]
-Re[be]=Pe[(xe+5)%Pe[ce]]}else if(we==f)Re[f]=Te[xe%Te[ce]]
-else if(we==i)-1==Xe[Ce](de)?Re[i]=Te[xe%Te[ce]]:Re[de]=xe%10
-else if(we==s)-1==Xe[Ce](ge)?Re[s]=Te[xe%Te[ce]]:Re[he]=Ue[(xe+25)%Ue[ce]]
-else if(we==$)Re[$]=me[xe%me[ce]]
-else{if(we!=n)return{ok:!1,[Ke]:Re,msg:Xe}
-Re[n]=""
-var Ae=e[pe](Re)[ze](De),Fe=""
-Ae[Ee](pullAns)
-Re[n]=Fe}je=(Xe=Je[pe](Re))[Ce](Ie)
-if(open_cnt(Xe)>ye||-1==je)break}}}
-for(let e=0;e<r.t[ce];e++)hack(r.t[e])}
+function(e,r){
+//	[{"locs":true,"tier":1,"type":"npc"},"floor","random","length","names","loosaz.temp {t:[","#s.","stringify","match",", ","]}","EZ_21","EZ_35","EZ_40","c001","c002","c003","l0cket","DATA_CHECK","green","lime","yellow","orange","red","purple","blue","cyan","open","release","unlock","6hh8xw","cmppiq","sa23uw","tvfkyq","uphlaw","vc2c7q","xwz7ja","pattern ","device","more","humor","angie's","mallory's","che ","will","faythe","hack","pet","safety","service","fact","capture","friend","th3","provides i","gc","CAFE","fran_lee","robovac","sentience","sans_comedy","angels","minions","sisters","petra","fountain","helpdesk","bunnybat","get_level","weathernet","eve","resource","bo","heard","teach","outta_juice","poetry","call","indexOf","LOCK_ERROR","includes","color_digit","c002_complement","c003_triad_1","c003_triad_2","digit","prime","ez_prime","split","\n","forEach","state"]
+    let[f,i,t,a,l,s,$,g,o,v,n,S,u,w,b,c,p,q,C,I,N,O,k,L,P,T,U,_,h,m,B,D,E,J,K,M,R,X,d,j,x,y,z,A,F,G,H,Q,V,W,Y,Z,ee,re,fe,ie,te,ae,le,se,$e,ge,oe,ve,ne,Se,ue,we,be,ce,pe,qe,Ce,Ie,Ne,Oe,ke,Le,Pe,Te,Ue,_e,he,me,Be,De,Ee,Je,Ke,Me,Re,Xe]=JSON.parse(#fs.scripts.quine().split`	`[1])
+    if(r[l]){var de=#fs.net.locs(f),je=s
+        for(let e=0;3>e;e++){je+=$+(xe=JSON[g](de)[o](/\b\w+\.\w+\b/g))[Math[i](Math[t]()*xe[a])]
+            2>e&&(je+=v)}return je+n}{let hack=e=>{var r=[S,u,w,b,c,p,q,C],f=[I,N,O,k,L,P,T,U],i=[_,h,m],t=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97],l=[B,D,E,J,K,M,R],s=[X,d,j,x,y,z,A,F,G,H,Q,V,W,Y,Z,ee,re,fe,ie,te],$=[ae,le,se,$e,ge,oe,ve,ne,Se,ue,we,be,ce,pe,qe,Ce,Ie,Ne,Oe,ke],g=e
+        let open_cnt=e=>(e[o](/LOCK_UNLOCKED/g)||[])[a]
+        for(var v={},n=g[Le](v),de=n[Pe](Te);;){if(-1==de)return[v,n]
+            for(var je="",xe=0;r[a]>xe;xe++)if(n[Pe](r[xe])>de){je=r[xe]
+                break}var ye=open_cnt(n)
+            for(xe=0;;xe++){let pullAns=e=>{for(var r=0;s[a]>r;r++)e[Ue](s[r])&&(Ae+=$[r])}
+                if(je==b){v[b]=f[xe%f[a]]
+                    v[_e]=v[b][a]}else if(je==c){v[c]=f[xe%f[a]]
+                    v[he]=f[(xe+4)%f[a]]}else if(je==p){v[p]=f[xe%f[a]]
+                    v[me]=f[(xe+3)%f[a]]
+                    v[Be]=f[(xe+5)%f[a]]}else if(je==S)v[S]=i[xe%i[a]]
+                else if(je==u)-1==n[Pe](De)?v[u]=i[xe%i[a]]:v[De]=xe%10
+                else if(je==w)-1==n[Pe](Ee)?v[w]=i[xe%i[a]]:v[Je]=t[(xe+25)%t[a]]
+                else if(je==q)v[q]=l[xe%l[a]]
+                else{if(je!=C)return{ok:!1,[Xe]:v,msg:n}
+                    v[C]=""
+                    var ze=e[Le](v)[Ke](Me),Ae=""
+                    ze[Re](pullAns)
+                    v[C]=Ae}de=(n=g[Le](v))[Pe](Te)
+                if(open_cnt(n)>ye||-1==de)break}}}
+        for(let e=0;e<r.t[a];e++)hack(r.t[e])}var xe}
