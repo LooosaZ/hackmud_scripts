13403de3623912e770864c4c807ec04ad16fe42c
LUSA <107564399+LooosaZ@users.noreply.github.com>
2024-05-07 12:00:46 +0100

    Update tier1.js
    un-minified the tier1 to have it as source

--- loosaz/tier1.js
+++ loosaz/tier1.js
@@ -1,26 +1,122 @@
-function(e,r){
-//	[{"locs":true,"tier":1,"type":"npc"},"floor","random","length","names","loosaz.temp {t:[","#s.","stringify","match",", ","]}","EZ_21","EZ_35","EZ_40","c001","c002","c003","l0cket","DATA_CHECK","green","lime","yellow","orange","red","purple","blue","cyan","open","release","unlock","6hh8xw","cmppiq","sa23uw","tvfkyq","uphlaw","vc2c7q","xwz7ja","pattern ","device","more","humor","angie's","mallory's","che ","will","faythe","hack","pet","safety","service","fact","capture","friend","th3","provides i","gc","CAFE","fran_lee","robovac","sentience","sans_comedy","angels","minions","sisters","petra","fountain","helpdesk","bunnybat","get_level","weathernet","eve","resource","bo","heard","teach","outta_juice","poetry","call","indexOf","LOCK_ERROR","includes","color_digit","c002_complement","c003_triad_1","c003_triad_2","digit","prime","ez_prime","split","\n","forEach","state"]
-    let[f,i,t,a,l,s,$,g,o,v,n,S,u,w,b,c,p,q,C,I,N,O,k,L,P,T,U,_,h,m,B,D,E,J,K,M,R,X,d,j,x,y,z,A,F,G,H,Q,V,W,Y,Z,ee,re,fe,ie,te,ae,le,se,$e,ge,oe,ve,ne,Se,ue,we,be,ce,pe,qe,Ce,Ie,Ne,Oe,ke,Le,Pe,Te,Ue,_e,he,me,Be,De,Ee,Je,Ke,Me,Re,Xe]=JSON.parse(#fs.scripts.quine().split`	`[1])
-    if(r[l]){var de=#fs.net.locs(f),je=s
-        for(let e=0;3>e;e++){je+=$+(xe=JSON[g](de)[o](/\b\w+\.\w+\b/g))[Math[i](Math[t]()*xe[a])]
-            2>e&&(je+=v)}return je+n}{let hack=e=>{var r=[S,u,w,b,c,p,q,C],f=[I,N,O,k,L,P,T,U],i=[_,h,m],t=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97],l=[B,D,E,J,K,M,R],s=[X,d,j,x,y,z,A,F,G,H,Q,V,W,Y,Z,ee,re,fe,ie,te],$=[ae,le,se,$e,ge,oe,ve,ne,Se,ue,we,be,ce,pe,qe,Ce,Ie,Ne,Oe,ke],g=e
-        let open_cnt=e=>(e[o](/LOCK_UNLOCKED/g)||[])[a]
-        for(var v={},n=g[Le](v),de=n[Pe](Te);;){if(-1==de)return[v,n]
-            for(var je="",xe=0;r[a]>xe;xe++)if(n[Pe](r[xe])>de){je=r[xe]
-                break}var ye=open_cnt(n)
-            for(xe=0;;xe++){let pullAns=e=>{for(var r=0;s[a]>r;r++)e[Ue](s[r])&&(Ae+=$[r])}
-                if(je==b){v[b]=f[xe%f[a]]
-                    v[_e]=v[b][a]}else if(je==c){v[c]=f[xe%f[a]]
-                    v[he]=f[(xe+4)%f[a]]}else if(je==p){v[p]=f[xe%f[a]]
-                    v[me]=f[(xe+3)%f[a]]
-                    v[Be]=f[(xe+5)%f[a]]}else if(je==S)v[S]=i[xe%i[a]]
-                else if(je==u)-1==n[Pe](De)?v[u]=i[xe%i[a]]:v[De]=xe%10
-                else if(je==w)-1==n[Pe](Ee)?v[w]=i[xe%i[a]]:v[Je]=t[(xe+25)%t[a]]
-                else if(je==q)v[q]=l[xe%l[a]]
-                else{if(je!=C)return{ok:!1,[Xe]:v,msg:n}
-                    v[C]=""
-                    var ze=e[Le](v)[Ke](Me),Ae=""
-                    ze[Re](pullAns)
-                    v[C]=Ae}de=(n=g[Le](v))[Pe](Te)
-                if(open_cnt(n)>ye||-1==de)break}}}
-        for(let e=0;e<r.t[a];e++)hack(r.t[e])}var xe}
+function(context, args) {//names:true
+
+    if(args.names) {
+        var resultado = #fs.net.locs({locs:true, tier:1, type:"npc"});
+
+        //return resultado;
+
+        var names = "loosaz.tier1 {t:[";
+
+        for (let i = 0; i < 3; i++) {
+            names += ("#s." + randomName(JSON.stringify(resultado).match(/\b\w+\.\w+\b/g)))
+            if(i < 2) names +=", "
+        }
+
+        return names + "]}";
+    } else {
+
+        for (let i = 0; i < args.t.length; i++) {
+            hack(args.t[i]);
+        }
+
+        function hack (args){
+            var LOCKS = ["EZ_21", "EZ_35", "EZ_40", "c001", "c002", "c003", "l0cket", "DATA_CHECK"]
+                , COLORS = ["green", "lime", "yellow", "orange", "red", "purple", "blue", "cyan"]
+                , EZ =  ["open", "release", "unlock"]
+                , PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+                , KEYS = ["6hh8xw", "cmppiq", "sa23uw", "tvfkyq", "uphlaw", "vc2c7q", "xwz7ja"]
+                , Q = ["pattern ","device","more","humor","angie's","mallory's","che ","will","faythe","hack","pet","safety","service","fact","capture","friend","th3","provides i","gc","CAFE"]
+                , A = ["fran_lee","robovac","sentience","sans_comedy","angels","minions","sisters","petra","fountain","helpdesk","bunnybat","get_level","weathernet","eve","resource","bo","heard","teach","outta_juice","poetry"]
+                , loc = args
+            let open_cnt = function(s) {
+                return (s.match(/LOCK_UNLOCKED/g) || []).length
+            }
+            var locks = {}
+                , hint = loc.call(locks)
+                , i_err = hint.indexOf("LOCK_ERROR")
+            while(true) {
+                if(i_err == -1)
+                    return [locks, hint]
+
+                var lk = "";
+                for(var i = 0; i < LOCKS.length; i++)
+                    if(hint.indexOf(LOCKS[i]) > i_err)
+                    {
+                        lk = LOCKS[i]
+                        break;
+                    }
+                var breached_locks = open_cnt(hint)
+                for(var i = 0; true; i++) {
+                    if(lk == "c001")
+                    {
+                        locks.c001 = COLORS[i%COLORS.length]
+                        locks.color_digit = locks.c001.length
+                    }
+                    else if(lk == "c002")
+                    {
+                        locks.c002 = COLORS[i%COLORS.length]
+                        locks.c002_complement = COLORS[(i+4)%COLORS.length]
+                    }
+                    else if(lk == "c003")
+                    {
+                        locks.c003 = COLORS[i%COLORS.length]
+                        locks.c003_triad_1 = COLORS[(i+3)%COLORS.length]
+                        locks.c003_triad_2 = COLORS[(i+5)%COLORS.length]
+                    }
+                    else if(lk == "EZ_21")
+                        locks.EZ_21 = EZ[i%EZ.length]
+                    else if(lk == "EZ_35")
+                    {
+                        if(hint.indexOf("digit") == -1)
+                            locks.EZ_35 = EZ[i%EZ.length]
+                        else
+                            locks.digit = i%10
+                    }
+                    else if(lk == "EZ_40")
+                    {
+                        if(hint.indexOf("prime") == -1)
+                            locks.EZ_40 = EZ[i%EZ.length]
+                        else
+                            locks.ez_prime = PRIMES[(i+25)%PRIMES.length]
+                    }
+                    else if(lk == "l0cket")
+                    {
+                        locks.l0cket = KEYS[i%KEYS.length]
+                    }
+                    else if(lk == "DATA_CHECK")
+                    {
+                        locks.DATA_CHECK=""
+                        var q = args.call(locks).split("\n")
+                        var ans = ""
+                        q.forEach(pullAns)
+                        locks.DATA_CHECK = ans
+                    }
+                    else
+                        return { ok:false, state:locks, msg:hint}
+
+                    hint = loc.call(locks);
+                    i_err = hint.indexOf("LOCK_ERROR")
+                    if(open_cnt(hint) > breached_locks || i_err == -1)
+                    {
+                        break;
+                    }
+                    function pullAns(q){
+                        for(var i = 0; i < Q.length; i++){
+                            if (q.includes(Q[i]))
+                            {
+                                ans += A[i]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    function randomName(array) {
+        let indiceAleatorio = Math.floor(Math.random() * array.length); // Gera um índice aleatório
+        return array[indiceAleatorio]; // Retorna o elemento no índice aleatório
+    }
+
+}
